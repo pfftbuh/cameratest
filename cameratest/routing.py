ca4cb84c033e43a91ca23e3b01b63315aaa3a408
pfftbuh <enkaiabiko@gmail.com>
2026-08-12 23:14:59 +0800

    new file:   cameratest/__init__.py 	new file:   cameratest/__pycache__/__init__.cpython-312.pyc 	new file:   cameratest/__pycache__/routing.cpython-312.pyc 	new file:   cameratest/__pycache__/settings.cpython-312.pyc 	new file:   cameratest/__pycache__/urls.cpython-312.pyc 	new file:   cameratest/__pycache__/wsgi.cpython-312.pyc 	new file:   cameratest/asgi.py 	new file:   cameratest/routing.py 	new file:   cameratest/settings.py 	new file:   cameratest/urls.py 	new file:   cameratest/wsgi.py 	new file:   db.sqlite3 	new file:   manage.py 	new file:   mycamera/__init__.py 	new file:   mycamera/__pycache__/__init__.cpython-312.pyc 	new file:   mycamera/__pycache__/admin.cpython-312.pyc 	new file:   mycamera/__pycache__/apps.cpython-312.pyc 	new file:   mycamera/__pycache__/consumers.cpython-312.pyc 	new file:   mycamera/__pycache__/models.cpython-312.pyc 	new file:   mycamera/__pycache__/routing.cpython-312.pyc 	new file:   mycamera/__pycache__/urls.cpython-312.pyc 	new file:   mycamera/__pycache__/views.cpython-312.pyc 	new file:   mycamera/admin.py 	new file:   mycamera/apps.py 	new file:   mycamera/consumers.py 	new file:   mycamera/migrations/__init__.py 	new file:   mycamera/migrations/__pycache__/__init__.cpython-312.pyc 	new file:   mycamera/models.py 	new file:   mycamera/routing.py 	new file:   mycamera/templates/mycamera/home.html 	new file:   mycamera/tests.py 	new file:   mycamera/urls.py 	new file:   mycamera/views.py

---
+++ cameratest/routing.py
@@ -0,0 +1,8 @@
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+import mycamera.routing
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": URLRouter(mycamera.routing.websocket_urlpatterns),
+})
